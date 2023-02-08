c11361415c48ea721c2afd622e29aaa21bd8ae96
Mauro Busso <mauro.busso12@gmail.com>
2023-02-08 15:56:53 +0000

    changes to file structure

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import Contacts from './components/Contacts'
 import Skills from './components/Skills'
 import Project from './components/Project'
 import About from './components/About'
-import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -20,8 +19,3 @@ root.render(
     <About />
   </>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
