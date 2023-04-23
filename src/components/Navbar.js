9e553c1d68beae2a1a16fd5ddf7b153e2f57355e
Mauro Busso <mauro.busso12@gmail.com>
2023-04-23 21:34:58 +0100

    changes to portfolio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,12 @@ export default function Navbar() {
       <>
         <nav className="flex ml-8 p-4">
           <div className="flex">
-            <p>M B</p>
+            <p></p>
             <ul className="inline-flex">
               <li className="ml-8">Contact</li>
               <li className="ml-8">Work</li>
               <li className="ml-8">About</li>
-              <li className="ml-8 bg-green-700 p-4 text-white">Resume</li>
+              <li className="ml-8 bg-green-700 p-4 text-white"><a href="#">Resume</a></li>
             </ul>
           </div>
         </nav>
--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Project() {
     return (
       <>
-        <div>
+        <div >
           <h1>title</h1>
           <img alt="" />
           <span>tech used</span>
