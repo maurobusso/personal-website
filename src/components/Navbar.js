709a74d54498437753e46cc49a08506d999b3c55
Mauro Busso <mauro.busso12@gmail.com>
2023-04-29 13:17:42 +0100

    navbar fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,14 @@ import React from "react"
 export default function Navbar() {
     return (
       <>
-        <nav className="flex ml-8 p-4">
-          <div className="flex">
+        <nav className="ml-8 p-4">
+          <div className="">
             <p></p>
-            <ul className="inline-flex">
-              <li className="ml-8">Contact</li>
-              <li className="ml-8">Work</li>
-              <li className="ml-8">About</li>
-              <li className="ml-8 bg-green-700 p-4 text-white"><a href="#">Resume</a></li>
+            <ul className="inline-flex items-center">
+              <li className="mx-4 my-5">Contact</li>
+              <li className="mx-4 my-5">Work</li>
+              <li className="mx-4 my-5">About</li>
+              <li className="mx-4 my-5 p-4 bg-green-700  text-white "><a href="#">Resume</a></li>
             </ul>
           </div>
         </nav>
