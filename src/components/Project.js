1cde27e5542ee71b163bb8bd7597a06d78028730
Mauro Busso <mauro.busso12@gmail.com>
2023-05-18 21:24:29 +0100

    fixed projects spacing

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,29 +6,37 @@ import project3 from "../Assets/projects-tumbnails/travel-journal.png"
 export default function Project() {
     return (
       <>
+        <span className="flex justify-center">My Projects</span>
         <div className="flex flex-col gap-10 mx-4 md:mx-10 lg:flex-row lg:flex-wrap">
-          <span className="text-center">My Projects</span>
-          <div className="basis-1/3 flex-1 m-8 ">
+          <div className="basis-1/3 flex-1 m-8 md:basis-1/4">
             <h1>Explore Space</h1>
-            <img src={project1} alt="" className="rounded-lg object-cover" ></img>
+            <a href="https://explore-space-app.netlify.app/">
+              <img src={project1} alt="" className="rounded-lg object-cover" ></img>
+            </a>
             <span>tech used</span>
             <div></div>
           </div>
-          <div className="basis-1/3 flex-1 m-8 ">
+          <div className="basis-1/3 flex-1 m-8 md:basis-1/4">
             <h1>Calculator for Local Anaesthetic</h1>
-            <img src={project2} alt="" className="rounded-lg object-cover" ></img>
+            <a href="https://msd-app.netlify.app/">
+              <img src={project2} alt="" className="rounded-lg object-cover" ></img>
+            </a>
             <span>tech used</span>
             <div></div>
           </div>
-          <div className="basis-1/3 flex-1 m-8 ">
+          <div className="basis-1/3 flex-1 m-8 md:basis-1/4">
             <h1>Travel Journal</h1>
-            <img src={project3} alt="" className="rounded-lg object-cover" ></img>
+            <a href="https://mauro-travel-journal.netlify.app/">
+              <img src={project3} alt="" className="rounded-lg object-cover" ></img>
+            </a>
             <span>tech used</span>
             <div></div>
           </div>
-          <div className="basis-1/3 flex-1 m-8 ">
-            <h1>Personal Portfolio</h1>
-            <img src="web1" alt="" className="rounded-lg object-cover" ></img>
+          <div className="basis-1/3 flex-1 m-8 md:basis-1/4">
+            <h1>Project</h1>
+            <a>
+              <img src="web1" alt="" className="rounded-lg object-cover" ></img>
+            </a>
             <span>tech used</span>
             <div></div>
           </div>  
