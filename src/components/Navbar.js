c2b867c230a23077a29f0d7e989644c64681ffe2
Mauro Busso <mauro.busso12@gmail.com>
2023-05-19 21:05:26 +0100

    fixed nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,15 @@ export default function Navbar() {
     return (
       <>
         <nav className="">
-          <div className="items-center">
+          <div className="">
             <p></p>
-            <ul className="flex items-center">
-              <li className="mx-4 my-5">
+            <ul className="flex items-center justify-center md:justify-end">
+              <li className="mx-4 my-5 md:mx-8">
                 <a href="/projects">Contact</a>
               </li>
-              <li className="mx-4 my-5">Work</li>
-              <li className="mx-4 my-5">About</li>
-              <li className="mx-4 my-5 p-4 bg-green-700 text-white "><a href="#">Resume</a></li>
+              <li className="mx-4 my-5 md:mx-8">Work</li>
+              <li className="mx-4 my-5 md:mx-8">About</li>
+              <li className="mx-4 my-5 p-4 bg-green-700 text-white md:mx-8"><a href="#">Resume</a></li>
             </ul>
           </div>
         </nav>
