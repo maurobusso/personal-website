d648c30d99e425c32478c362a0634f84003ed982
Mauro Busso <mauro.busso12@gmail.com>
2023-06-15 19:55:52 +0100

    changes to colour

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,7 +10,7 @@ export default function Main() {
   return (
     <>
       <div className="m-10 items-center justify-center md:m-20 lg-60 md:text-center">
-          <h1 className="font-Fredericka text-3xl text-green-600 underline my-20">
+          <h1 className="font-Fredericka text-3xl text-yellow-600 underline my-20">
             Hello, I am Mauro
           </h1>
           <div className="my-8 font-SpecialElite text-lg">
@@ -21,7 +21,7 @@ export default function Main() {
               I'm a front-end developer with a passion for building apps and websites. I love to problem solve and think outside of the box. A strong background in Health care has taught me the importance of constantly learning and improving, and how to communicate effectively. I am highly passionate about advancing my development career in tech and I am open to new opportunites!
             </span>
             <div className="relative mx-auto w-60 h-60 my-20 rounded-xl lg:w-80 lg:h-80"> 
-              <div className="absolute -right-8 -top-12 bg-green-700 w-60 h-60 my-20 rounded-xl lg:w-80 lg:h-80"></div>
+              <div className="absolute -right-8 -top-12 bg-yellow-600 w-60 h-60 my-20 rounded-xl lg:w-80 lg:h-80"></div>
               <div className="relative mx-auto w-60 h-60 my-20 rounded-xl lg:w-80 lg:h-80">
                 <img src={myImg} alt="img" layout="fill" className="object-cover w-full h-auto rounded-xl"></img>
               </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,14 +9,23 @@ import react from "../Assets/icons/react.png"
 import js from "../Assets/icons/js.png"
 import api from "../Assets/icons/api-64.png"
 
+//data that i need from every project
+//title, img, logo of techs used, description
+{/* <Project
+  title=
+  img=
+  tech=
+  description=
+> */}
+
 
 export default function Project() {
     return (
       <>
-        <span className="flex justify-center mt-40 mb-20 font-Fredericka text-3xl" id="project">My Projects</span>
+        <h2 className="flex justify-center mt-40 mb-20 font-Fredericka text-3xl text-yellow-600" id="project">My Projects</h2>
         <span className="flex justify-center px-10 font-SpecialElite">Click on any project to learn more</span>
         <div className="flex flex-col gap-10 mx-4 md:mx-10 lg:flex-row lg:flex-wrap">
-          <div className="flex-1 m-8 md:basis-1/4 shadow-2xl rounded-2xl p-4">
+          <div className="flex-1 m-8 md:basis-1/4 shadow-2xl shadow-yellow-600  rounded-2xl p-4">
             <h1 className="my-4 font-SpecialElite">Explore Space</h1> 
             <div className="flex justify-center content-center">
               <a href="https://explore-space-app.netlify.app/">
@@ -62,7 +71,7 @@ export default function Project() {
             </div>
             <div className="my-4 font-SpecialElite">This is an app that allow the user to fetch data from various NASA APIs and display images and informations about our amazing universe. In the first section the app retrieve information regarding asteroids near our planet, in the other two section an image or photograph of our universe is fetured.</div>
           </div>
-          <div className=" flex-1 m-8 md:basis-1/4 shadow-2xl rounded-2xl p-4">
+          <div className=" flex-1 m-8 md:basis-1/4 shadow-2xl shadow-yellow-600 rounded-2xl p-4">
             <h1 className="my-4 font-SpecialElite">Calculator for Local Anaesthetic</h1>
             <div className="flex justify-center content-center">
               <a href="https://msd-app.netlify.app/">
@@ -101,7 +110,7 @@ export default function Project() {
             </div>
             <div className="my-4 font-SpecialElite">This app was made as a tool to be used by podiatrist when calculating the amount of local anaesthetic to safely inject into a patient. This is because for experience, i know that most podiatry do not remember the exact mathematical equation and they always have to look it up and manually calculate the dose, which is not ideal. Taking up time that can be used more efficiently.</div>
           </div>
-          <div className="flex-1 m-8 md:basis-1/4 shadow-2xl rounded-2xl p-4">
+          <div className="flex-1 m-8 md:basis-1/4 shadow-2xl shadow-yellow-600  rounded-2xl p-4">
             <h1 className="my-4 font-SpecialElite">Travel Journal</h1>
             <div className="flex justify-center content-center">
               <a href="https://mauro-travel-journal.netlify.app/">
@@ -141,7 +150,7 @@ export default function Project() {
             <div className="my-4 font-SpecialElite">This project was inspired by the Scrimba react course as a final solo project at the end of the second module of the course. It is a static page that display different location with immages, a brief description and a link to the google map location.
             </div>
           </div>
-          <div className="flex-1 m-8 md:basis-1/4 shadow-2xl rounded-2xl p-4">
+          <div className="flex-1 m-8 md:basis-1/4 shadow-2xl shadow-yellow-600  rounded-2xl p-4">
             <h1 className="my-4 font-SpecialElite">Blackjack</h1>
             <div className="flex justify-center content-center">
               <a href="https://blackjack-game-mauro.netlify.app/">
